879968878642b0ea0e7e1c944d5e538da09a5a30
martin <118768668+MartinBersh@users.noreply.github.com>
2023-06-04 13:56:27 -0500

    commit

--- src/main/java/com/example/ejemplocrud/HelloApplication.java
+++ src/main/java/com/example/ejemplocrud/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainPanel.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainPanel2.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 720, 320);
         stage.setTitle("CRUD");
         stage.setScene(scene);
---
+++ src/main/java/library/Horarios.java
@@ -0,0 +1,40 @@
+package library;
+
+public class Horarios {
+    private int id;
+    private String nombreprofesor;
+    private String aula;
+    private String horario;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreprofesor() {
+        return nombreprofesor;
+    }
+
+    public void setNombreprofesor(String nombreprofesor) {
+        this.nombreprofesor = nombreprofesor;
+    }
+
+    public String getAula() {
+        return aula;
+    }
+
+    public void setAula(String aula) {
+        this.aula = aula;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+}
